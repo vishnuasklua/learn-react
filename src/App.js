43b8f7a0d8f687b3577356beed63b089a7d3472b
vishnu-SAO <vishnu@asklua.com>
2019-12-06 15:02:24 +0530

    initialize repo

---
+++ src/App.js
@@ -0,0 +1,27 @@
+const Pet = props => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ]);
+};
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Learn about it!"),
+    React.createElement(Pet, {
+      name: "rom",
+      animal: "trex",
+      breed: "dinosaur"
+    }),
+    React.createElement(Pet, {
+      name: "rom",
+      animal: "trex",
+      breed: "dinosaur"
+    })
+  );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
